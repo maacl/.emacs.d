d6c9c41cd327cc16bcd755d96bb044e4d71587c3
Martin Clausen <martin.clausen@gmail.com>
2016-01-09 15:19:17 +0100

    Initial commit

---
+++ init.el
@@ -0,0 +1,205 @@
+;; Package management
+(require 'package)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(package-initialize)
+(require 'cask "/usr/local/share/emacs/site-lisp/cask/cask.el")
+(cask-initialize)
+(require 'pallet)
+(pallet-mode t)
+
+;; Constants
+(defconst my/home (getenv "HOME"))
+(defconst my/hostname system-name)
+(defconst my/bin (concat my/home "/bin"))
+(defconst my/emacs.d (concat my/home "/.emacs.d/"))
+
+;; set OSX specific stuff - path and font
+(defun set-exec-path-from-shell-PATH ()
+  "Set up Emacs' `exec-path' and PATH environment variable to match that used by the user's shell.
+   This is particularly useful under Mac OSX, where GUI apps are not started from a shell."
+  (interactive)
+  (let ((path-from-shell (replace-regexp-in-string "[ \t\n]*$" "" (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+
+(set-exec-path-from-shell-PATH)
+
+(when (equal system-type 'darwin) 
+  (set-frame-font "Menlo-13"))
+
+
+;; Add personal bin to path
+(add-to-list 'exec-path my/bin)
+
+;; Terminal specific key codes
+(define-key input-decode-map "\e[1;10A" [M-S-up])
+(define-key input-decode-map "\e[1;10B" [M-S-down])
+(define-key input-decode-map "\e[1;10C" [M-S-right])
+(define-key input-decode-map "\e[1;10D" [M-S-left])
+
+;; UI
+;;(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(set-fringe-style -1)
+;;(linum-mode 1)
+
+(setq inhibit-splash-screen t)
+
+(setq visible-bell 1)
+
+(blink-cursor-mode -1)
+
+(global-hl-line-mode)
+
+(require 'popwin)
+(popwin-mode 1)
+
+;; Themes
+
+(defconst my/light-theme 'plan9)
+(defconst my/dark-theme 'darktooth)
+
+(defun my/change-theme (old new)
+  (disable-theme old)
+  (load-theme new t))
+
+(defun my/dark ()
+  (interactive)
+  (my/change-theme my/light-theme my/dark-theme))
+
+(defun my/light ()
+  (interactive)
+  (my/change-theme my/dark-theme my/light-theme))
+
+(my/light)
+
+(global-set-key (kbd "C-x d") 'my/dark)
+(global-set-key (kbd "C-x l") 'my/light)
+
+
+;; Commands
+(require 'ido)
+(ido-mode)
+;;; Ignore .DS_Store files with ido mode
+(add-to-list 'ido-ignore-files "\\.DS_Store")
+
+
+(require 'ido-vertical-mode)
+(ido-vertical-mode)
+
+(setq ido-enable-flex-matching t)
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;;Editor
+(setq make-backup-files nil)
+
+(delete-selection-mode)
+
+(setq whitespace-style '(face trailing tabs))
+
+(add-hook 'prog-mode-hook 'whitespace-mode)
+
+(setq tab-width 4)
+
+(require 'ag)
+(setq ag-reuse-buffers 't)
+
+(global-set-key (kbd "C-c C-g") 'ag)
+
+;; Autocompletion
+(require 'company)
+(add-hook 'after-init-hook 'global-company-mode)
+(company-quickhelp-mode 1)
+
+;;ERC
+(require 'erc)
+
+(require 'erc-hl-nicks)
+(erc-hl-nicks-enable)
+
+(require 'erc-image)
+(erc-image-enable)
+
+(add-to-list 'erc-modules 'notifications)
+
+;;Programming
+(add-hook 'prog-mode-hook 'show-paren-mode)
+
+(setq show-paren-style 'expression)
+
+(require 'rainbow-delimiters)
+
+(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+
+(add-hook 'prog-mode-hook 'electric-pair-mode)
+
+;; Clojure
+(require 'cider)
+
+(setq nrepl-hide-special-buffers t
+      cider-repl-pop-to-buffer-on-connect nil
+      cider-popup-stacktraces nil
+      cider-repl-popup-stacktraces t)
+
+(setq cider-repl-result-prefix ";; => ")
+
+(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
+
+(setq org-babel-clojure-backend 'cider)
+
+;; Magit
+(global-set-key (kbd "C-x g") 'magit-status)
+
+;; Org
+(require 'org)
+
+(setq org-return-follows-link t)
+
+;; Babel
+
+(defconst my/babel-languages '(emacs-lisp sh js haskell python clojure))
+
+(org-babel-do-load-languages
+  'org-babel-load-languages
+   (mapcar (lambda (lang) (cons lang t)) my/babel-languages))
+
+(setq org-src-tab-acts-natively t
+      org-src-fontify-natively t
+      org-confirm-babel-evaluate nil)
+;; Tasks
+
+(defconst my/org-keywords
+  '((sequence "TODO(t)" "INPROGRESS(i)" "WAIT(w)" "|" "DONE(d)")))
+
+(setq org-todo-keywords my/org-keywords)
+
+(setq org-enforce-todo-dependencies t)
+
+(org-display-inline-images)
+
+(require 'org-bullets)
+
+(add-hook 'org-mode-hook 'org-bullets-mode)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(fci-rule-color "#f8fce8")
+ '(hl-paren-background-colors (quote ("#e8fce8" "#c1e7f8" "#f8e8e8")))
+ '(hl-paren-colors (quote ("#40883f" "#0287c8" "#b85c57")))
+ '(package-selected-packages
+   (quote
+    (org pos-tip use-package smex smartparens slime rainbow-delimiters popwin plan9-theme pallet org-tree-slide org-bullets multiple-cursors markdown-mode magit ido-vertical-mode idle-highlight-mode flycheck fancy-narrow expand-region erc-image erc-hl-nicks darktooth-theme company-quickhelp cider ag adoc-mode)))
+ '(sml/active-background-color "#98ece8")
+ '(sml/active-foreground-color "#282828")
+ '(sml/inactive-background-color "#4fa8a8")
+ '(sml/inactive-foreground-color "#282828"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
